950b5d3864475c6aecc8bf2185a19a6c03824b76
Lorelei Wright <wrighl@rpi.edu>
2018-10-08 16:13:46 -0400

    Added better swipe detection, and uses the organ object

--- Phaser Game/js/autopsy.js
+++ Phaser Game/js/autopsy.js
@@ -5,27 +5,28 @@ let autopsyState = function ()
 
 autopsyState.prototype.create = function ()
 {
+    game.debug.start();
     this.empty_body = game.add.sprite(0, 0, "body");
 
-    this.heart = new Heart(500, 1000);
+    this.heart = new Organ(500, 1000, "heart", [{ x:199, y:0, angle:0, intact: true },{ x:0, y:200, angle:Math.PI/2, intact: true}]);
     game.add.existing(this.heart);
 
-    game.input.onDown.add(this.start_cut, this);
-    game.input.onUp.add(this.end_cut, this);
-
-    this.cut_start = { x:0, y:0 };
-    this.cut_end   = { x:0, y:0 };
+    this.cut = new Phaser.Line(0,0,0,0);
 };
 
-autopsyState.prototype.start_cut = function (pos)
+autopsyState.prototype.update = function ()
 {
-    this.cut_start.x = pos.x;
-    this.cut_start.y = pos.y;
-};
-
-autopsyState.prototype.end_cut = function (pos)
-{
-    this.cut_end.x = pos.x;
-    this.cut_end.y = pos.y;
-    this.heart.check_cut(new Phaser.Line(this.cut_start.x, this.cut_start.y, this.cut_end.x, this.cut_end.y));
+    // check for a swipe -- Inspired by https://gist.github.com/eguneys/5cf315287f9fbf413769
+    swipe_length = Phaser.Point.distance(game.input.activePointer.position, game.input.activePointer.positionDown);
+    swipe_time = game.input.activePointer.duration;
+    if (swipe_length > 100 && swipe_time > -1 && swipe_time < 250)
+    {
+        this.cut = new Phaser.Line(game.input.activePointer.positionDown.x, game.input.activePointer.positionDown.y,
+            game.input.activePointer.position.x, game.input.activePointer.position.y);
+        if (this.heart.check_cut(this.cut))
+        {
+            this.heart.inputEnabled = true;
+            this.heart.input.enableDrag(true);
+        }
+    }   
 };

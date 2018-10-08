91b336396f33475c3d514ede7e1317c2c3fbc8e5
Lorelei Wright <wrighl@rpi.edu>
2018-10-08 17:02:38 -0400

    updated cut detection so it's not so redundant

--- Phaser Game/js/organ.js
+++ Phaser Game/js/organ.js
@@ -6,6 +6,8 @@ let Organ = function (x, y, sprite, cuts)
     // angles are in radians in the range [0,2*PI)
     this.cuts = cuts;
 
+    this.free = false;
+
     this.cut_distance_tolerance = 40;
     this.cut_angle_tolerange = Math.PI/12;
 };
@@ -21,51 +23,57 @@ Organ.prototype.check_cut = function (cut)
 {
     // check collision against every "collider" (maybe just check if the swipe is close enough and
     // the angle of the swipe is close to what's expected)
-    for (i = 0; i < this.cuts.length; i++)
+    if (!this.free)
     {
-        cut_point = new Phaser.Point(this.cuts[i].x + this.position.x, this.cuts[i].y + this.position.y);
-
-        // do the math
-        cut_p = Phaser.Point.subtract(cut.end, cut.start);
-        proj_p = Phaser.Point.subtract(cut_point, cut.start);
-        final_p = Phaser.Point.project(proj_p, cut_p);
-        closest = Phaser.Point.add(final_p, cut.start);
-        closest.clampX(cut.left, cut.right).clampY(cut.top, cut.bottom);
-
-        distance = Phaser.Point.distance(closest, cut_point);
-
-        within_distance = (distance < this.cut_distance_tolerance);
-        within_angle1 = (Phaser.Math.difference(
-            Phaser.Math.normalizeAngle(cut.angle), 
-            Phaser.Math.normalizeAngle(this.cuts[i].angle)
-            ) < this.cut_angle_tolerange);
-        within_angle2 = (Phaser.Math.difference(
-            Phaser.Math.normalizeAngle(cut.angle), 
-            Phaser.Math.normalizeAngle(this.cuts[i].angle + Math.PI)
-            ) < this.cut_angle_tolerange);
-        within_angle3 = (Phaser.Math.difference(
-            Phaser.Math.normalizeAngle(cut.angle + Math.PI), 
-            Phaser.Math.normalizeAngle(this.cuts[i].angle)
-            ) < this.cut_angle_tolerange);
-        within_angle4 = (Phaser.Math.difference(
-            Phaser.Math.normalizeAngle(cut.angle + Math.PI), 
-            Phaser.Math.normalizeAngle(this.cuts[i].angle + Math.PI)
-            ) < this.cut_angle_tolerange);
-
-        if (within_distance && (within_angle1 || within_angle2 || within_angle3 || within_angle4))
+        for (i = 0; i < this.cuts.length; i++)
         {
-            console.log("CUT");
-            this.cuts[i].intact = false;
+            cut_point = new Phaser.Point(this.cuts[i].x + this.position.x, this.cuts[i].y + this.position.y);
+
+            // do the math
+            cut_p = Phaser.Point.subtract(cut.end, cut.start);
+            proj_p = Phaser.Point.subtract(cut_point, cut.start);
+            final_p = Phaser.Point.project(proj_p, cut_p);
+            closest = Phaser.Point.add(final_p, cut.start);
+            closest.clampX(cut.left, cut.right).clampY(cut.top, cut.bottom);
+
+            distance = Phaser.Point.distance(closest, cut_point);
+
+            within_distance = (distance < this.cut_distance_tolerance);
+            within_angle1 = (Phaser.Math.difference(
+                Phaser.Math.normalizeAngle(cut.angle), 
+                Phaser.Math.normalizeAngle(this.cuts[i].angle)
+                ) < this.cut_angle_tolerange);
+            within_angle2 = (Phaser.Math.difference(
+                Phaser.Math.normalizeAngle(cut.angle), 
+                Phaser.Math.normalizeAngle(this.cuts[i].angle + Math.PI)
+                ) < this.cut_angle_tolerange);
+            within_angle3 = (Phaser.Math.difference(
+                Phaser.Math.normalizeAngle(cut.angle + Math.PI), 
+                Phaser.Math.normalizeAngle(this.cuts[i].angle)
+                ) < this.cut_angle_tolerange);
+            within_angle4 = (Phaser.Math.difference(
+                Phaser.Math.normalizeAngle(cut.angle + Math.PI), 
+                Phaser.Math.normalizeAngle(this.cuts[i].angle + Math.PI)
+                ) < this.cut_angle_tolerange);
+
+            if (within_distance && (within_angle1 || within_angle2 || within_angle3 || within_angle4))
+            {
+                this.cuts[i].intact = false;
+            }
         }
-        
-        //game.debug.geom(new Phaser.Circle(closest.x, closest.y, 4), 'blue', true);
-        //game.debug.geom(new Phaser.Circle(final_p.x + cut.x1, final_p.y + cut.y1, 5), 'black', true);
+        this.free = true;
+        for (i = 0; i < this.cuts.length; i++)
+        {
+            if (this.cuts[i].intact) this.free = false;
+        }
+        if (this.free)
+        {
+            this.inputEnabled = true;
+            this.input.enableDrag(true);
+        }
     }
-    for (i = 0; i < this.cuts.length; i++)
-    {
-        if (this.cuts[i].intact == true) return false;
-    }
-    return true;
+    console.log(this.free);
+    return this.free;
 }
 
 Organ.prototype.update = function ()

de083defcd7606c53c0dbb4febe5430280809df7
Jerry <jerry.s.jacobs@gmail.com>
2018-10-15 20:06:29 -0400

    start pressing

--- Phaser Game/js/menu.js
+++ Phaser Game/js/menu.js
@@ -14,6 +14,7 @@ menu.prototype.create = function(){
 menu.prototype.update = function(){
 
     this.startButton.inputEnabled = true; 
+    this.startButton.events.onInputDown.add(this.buttonDown, this);
     this.startButton.events.onInputUp.add(this.startGame);
 
 }
@@ -21,4 +22,10 @@ menu.prototype.update = function(){
 menu.prototype.startGame = function(){
     game.state.start("Game");
 
+}
+
+menu.prototype.buttonDown = function(sprite){
+	sprite.scale.set(.8,.8);
+	sprite.x += sprite.width*.1;
+	sprite.y += sprite.height*.1;
 }

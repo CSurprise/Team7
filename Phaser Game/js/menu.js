dfa2fed76110e94ed3dfe52c200a19c532e72724
Jerry <jerry.s.jacobs@gmail.com>
2018-10-15 22:59:55 -0400

    configured intro text

--- Phaser Game/js/menu.js
+++ Phaser Game/js/menu.js
@@ -15,18 +15,26 @@ menu.prototype.create = function(){
     this.overlay.inputEnabled = true;
     this.overlay.events.onInputDown.add(this.startGame, this);
 
-    this.string = "Intro Text"
-    this.text = game.add.existing(new Phaser.Text(game, 200, 600, this.string, {
+    this.string = "The year is 1949, you are a medical student with a month left in the "+
+    "Pathology program of the Tan Tek Guanmedical school. Your last "+
+    "assignment to become a doctor is to identify the cause/causes of "+
+    "death of a recently deceased man born and raised in the rural parts "+
+    "of Singapore.\n\n\n\n\n\nWarning\n"+
+    "This game was made by college students, not doctors. Please do not "
+    "try to diagnose anyone with these diseases."
+    
+    this.text = game.add.existing(new Phaser.Text(game, 150, 300, this.string, {
         font:'bold 50pt Arial',
         fill: "#ffffff",
 		wordWrap:true,
-        wordWrapWidth:700
+        wordWrapWidth:900,
+        align:'center'
     }));
     this.text.visible = false;
 };
 
 menu.prototype.update = function(){
-
+    
 };
 
 menu.prototype.showOverlay = function(sprite, pointer){

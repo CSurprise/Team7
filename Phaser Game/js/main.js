63bdea131cd1932fae8bd1d7e404d19676ac9d80
Giovanni Morales <GiovanniMorales021@gmail.com>
2018-10-18 13:56:31 -0400

    formatting for website

--- Phaser Game/js/main.js
+++ Phaser Game/js/main.js
@@ -1,4 +1,10 @@
 let game = new Phaser.Game(1125, 2436, Phaser.AUTO);
+function init() {
+game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+game.scale.pageAlignVertically = true;
+game.scale.pageAlignHorizontally = true;
+}
+
 
 game.state.add("Preload", preloadState);
 game.state.add("Autopsy", autopsyState);
